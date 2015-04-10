671eae598674f3f8d205ea92f21bc5b3ff1ac987
dingy007 <dingy007@gmail.com>
2015-04-10 12:53:39 +0800

    Adding Hibernate Utils file

---
+++ src/main/java/com/koreinfotech/sms/utilities/HibernateUtils.java
@@ -0,0 +1,35 @@
+package com.koreinfotech.sms.utilities;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+	private static SessionFactory sessionFactory = null;
+	
+	
+	public static SessionFactory getSessionFactory() {
+		if (sessionFactory==null) {
+			Configuration configuration = new Configuration();
+			configuration.configure("hibernate.cfg.xml");
+			StandardServiceRegistryBuilder standardServiceRegistryBuilder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+			sessionFactory = configuration.buildSessionFactory(standardServiceRegistryBuilder.build());
+		}
+		return sessionFactory;
+	}
+	
+	public static Session getCurrentSession() {
+		return getSessionFactory().getCurrentSession();
+	}
+	
+	public static Session openSession() {
+		return getSessionFactory().openSession();
+	}
+	
+	public static void closeSession(Session session){
+		if (session.isOpen()){
+			session.close();
+		}
+	}
+}
